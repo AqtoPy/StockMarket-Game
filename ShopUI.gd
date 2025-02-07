8c7682284ce782ebca583f3982c0e44b7c5cc9d8
AqtoPy <rekikreki66@gmail.com>
2025-02-07 22:13:48 +0300

    Update ShopUI.gd

--- ShopUI.gd
+++ ShopUI.gd
@@ -3,9 +3,19 @@ extends Control
 @onready var money_label = $MoneyLabel
 @onready var crystals_label = $CrystalsLabel
 @onready var message_label = $MessageLabel
+@onready var item_list = $ItemList  # ItemList для отображения товаров
+
+var items = [
+    {"name": "Карандаш", "price": 10},
+    {"name": "Ручка", "price": 50},
+    {"name": "Корректор", "price": 100},
+    {"name": "Зарядный провод", "price": 500},
+    {"name": "Флешка", "price": 1000}
+]
 
 func _ready():
     update_ui()
+    populate_item_list()
 
 func update_ui():
     money_label.text = "Деньги: " + str(PlayerData.money) + " ₽"
@@ -14,18 +24,21 @@ func update_ui():
 func show_message(message):
     message_label.text = message
 
-func _on_buy_item_button_pressed(item_name, item_price):
-    if PlayerData.money >= item_price:
-        PlayerData.add_item(item_name, item_price)
-        update_ui()
-        show_message("Куплен товар: " + item_name)
-    else:
-        show_message("Недостаточно денег!")
+func populate_item_list():
+    item_list.clear()
+    for item in items:
+        item_list.add_item(item["name"] + " - " + str(item["price"]) + " ₽")
 
-func _on_buy_upgrade_button_pressed(upgrade_cost):
-    if PlayerData.crystals >= upgrade_cost:
-        PlayerData.crystals -= upgrade_cost
-        show_message("Улучшение куплено!")
-        update_ui()
+func _on_buy_item_button_pressed():
+    var selected_item_index = item_list.get_selected_items()
+    if selected_item_index.size() > 0:
+        var item = items[selected_item_index[0]]
+        if PlayerData.money >= item["price"]:
+            PlayerData.money -= item["price"]
+            PlayerData.inventory.append(item["name"])
+            update_ui()
+            show_message("Куплен товар: " + item["name"])
+        else:
+            show_message("Недостаточно денег!")
     else:
-        show_message("Недостаточно кристаллов!")
+        show_message("Выберите товар!")
